cc23d123464da06c6e06fbf8f23755d2c5193607
gtankha <46304828+gtankha@users.noreply.github.com>
2020-09-28 01:33:49 -0700

    Add Heroku id

--- server.js
+++ server.js
@@ -55,12 +55,16 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
   }
-  
+
 
 app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-      res.json(result);
-  });
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 
 app.listen(PORT, () => {
